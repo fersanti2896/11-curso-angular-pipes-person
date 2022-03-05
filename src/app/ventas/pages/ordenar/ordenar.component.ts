04fe969cf9bd0438b11f88d49bead209f05f880f
Fernando Nicolas <fersanti2896@gmail.com>
2022-03-05 17:59:11 -0600

    Crando interface Heroe y mostrando la data en el TablePrime

--- src/app/ventas/pages/ordenar/ordenar.component.ts
+++ src/app/ventas/pages/ordenar/ordenar.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Color, Heroe } from '../../interfaces/ventas.interface';
 
 @Component({
   selector: 'app-ordenar',
@@ -8,6 +9,28 @@ import { Component } from '@angular/core';
 })
 export class OrdenarComponent {
   enMayusculas: boolean = true;
+  heroes: Heroe[] = [
+    {
+      nombre: 'Superman',
+      vuela: true,
+      color: Color.azul
+    },
+    {
+      nombre: 'Batman',
+      vuela: false,
+      color: Color.negro
+    },
+    {
+      nombre: 'Robin',
+      vuela: false,
+      color: Color.verde
+    },
+    {
+      nombre: 'Linterna Verde',
+      vuela: true,
+      color: Color.verde
+    }
+  ]
 
   cambiarMay() {
     this.enMayusculas = !this.enMayusculas;
